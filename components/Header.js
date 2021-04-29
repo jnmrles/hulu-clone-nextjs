4ff325bde18442d59a26a4a5169342ecb5471cb2
JM <jnmrles@gmail.com>
2021-04-29 03:54:04 -0500

    development progress on Header component. Updated Global CSS

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,28 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+import Image from "next/image";
+import HeaderItem from "./HeaderItem.js";
 function Header() {
-  return <div></div>;
+  return (
+    <header className="">
+      <div>
+        <HeaderItem title="Home" Icon="" />
+      </div>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+      />
+    </header>
+  );
 }
 
 export default Header;
