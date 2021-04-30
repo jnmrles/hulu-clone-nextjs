a2cf806095211108128af368ddddb922c8991cc1
JM <jnmrles@gmail.com>
2021-04-30 04:59:11 -0500

    developed starting component for movie thumbnails

--- components/ThumbNail.js
+++ components/ThumbNail.js
@@ -1,7 +1,29 @@
-function ThumbNail() {
+import { ThumbUpIcon } from "@heroicons/react/outline";
+import Image from "next/image";
+import { forwardRef } from "react";
+
+function ThumbNail({ result }) {
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
     <div>
-      <h1>thumbanil</h1>
+      <Image
+        layout="responsive"
+        height={1080}
+        width={1920}
+        src={
+          `${BASE_URL}${result.backdrop_path || result.poster_path} ` ||
+          `${BASE_URL}${result.poster_path}`
+        }
+      />
+      <div className="p-2">
+        <p className="truncate max-w-md">{result.overview}</p>
+        <h2>{result.title || result.original_name}</h2>
+        <p>
+          {result.media_type && `${result.media_type} •`}{" "}
+          {result.release_date || result.first_air_date} •{" "}
+          <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
+        </p>
+      </div>
     </div>
   );
 }
