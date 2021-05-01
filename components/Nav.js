578ea7ed94984b44781b7e3b3083a787d2fefc5b
JM <jnmrles@gmail.com>
2021-05-01 02:22:35 -0500

    changes

--- components/Nav.js
+++ components/Nav.js
@@ -1,14 +1,12 @@
-import requests from "../utils/requests";
 import { useRouter } from "next/router";
+import requests from "../utils/requests";
 
 function Nav() {
   const router = useRouter();
+
   return (
     <nav className="relative">
-      <div
-        className="flex sm:px-20 text-2xl whitespace-nowrap
-        space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide"
-      >
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
