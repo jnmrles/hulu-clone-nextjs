cba86a5b1f05d67fcd7d8314c866642bb7ebc6bf
JM <jnmrles@gmail.com>
2021-05-01 02:12:20 -0500

    added flip move animation, map error fix

--- components/Results.js
+++ components/Results.js
@@ -1,14 +1,14 @@
-import ThumbNail from "./ThumbNail";
+import Thumbnail from "./Thumbnail";
 import FlipMove from "react-flip-move";
 
-const Results = ({ results }) => {
+function Results({ results }) {
   return (
-    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map((result) => (
-        <ThumbNail key={result.id} result={result} />
+        <Thumbnail key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   );
-};
+}
 
 export default Results;
